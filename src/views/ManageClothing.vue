48a2b78458a0c452f57aa69a46e461ad0eb78308
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-23 11:36:47 -0700

    Changed 5 seconds in timeout to 4 seconds.

--- src/views/ManageClothing.vue
+++ src/views/ManageClothing.vue
@@ -80,7 +80,7 @@ export default {
                 .then(() => {
                     setTimeout(function(){
                         this.message = ""
-                    }.bind(this), 5000)
+                    }.bind(this), 4000)
                 })
         },
         updateItem(id, data){
@@ -93,7 +93,7 @@ export default {
                 .then(() => {
                     setTimeout(function(){
                         this.message = ""
-                    }.bind(this), 5000)
+                    }.bind(this), 4000)
                 })
         },
         removeItem(id){
